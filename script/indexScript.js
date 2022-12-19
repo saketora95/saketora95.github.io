97f7fabc15fbcb49d55789677a2b238437670bfd
saketora95 <jakeasa123@gmail.com>
2022-12-19 10:42:04 +0800

    form test

--- script/indexScript.js
+++ script/indexScript.js
@@ -1,6 +1,6 @@
 function initialURLInput() {
 
     // 2022-12-15
-    document.getElementById('source_URL').value = window.location.href
+    document.getElementById('source_url').value = window.location.href
 
 }
